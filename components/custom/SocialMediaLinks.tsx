2beb19e491fb050879236fc5d58abdca22aba57c
GRAMAPHENIA <dicoratojuanpablo@gmail.com>
2024-10-26 11:16:27 -0300

    fix-one-?

--- components/custom/SocialMediaLinks.tsx
+++ components/custom/SocialMediaLinks.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { BsInstagram, BsFacebook, BsYoutube } from "react-icons/bs";
+// import React from "react";
+// import { BsInstagram, BsFacebook, BsYoutube } from "react-icons/bs";
 
-const socialMedia = [
-  {
-    icon: BsInstagram,
-    href: "https://www.instagram.com/elmetodoadulma/",
-    size: 50,
-  },
-  {
-    icon: BsFacebook,
-    href: "https://www.facebook.com/El.metodo.adulma",
-    size: 50,
-  },
-  {
-    icon: BsYoutube,
-    href: "https://www.youtube.com/channel/UCWVEYCAyE61A9bBvWVNLebQ",
-    size: 60,
-  },
-];
+// const socialMedia = [
+//   {
+//     icon: BsInstagram,
+//     href: "https://www.instagram.com/elmetodoadulma/",
+//     size: 50,
+//   },
+//   {
+//     icon: BsFacebook,
+//     href: "https://www.facebook.com/El.metodo.adulma",
+//     size: 50,
+//   },
+//   {
+//     icon: BsYoutube,
+//     href: "https://www.youtube.com/channel/UCWVEYCAyE61A9bBvWVNLebQ",
+//     size: 60,
+//   },
+// ];
 
-export const SocialMediaLinks: React.FC = () => (
-  <div className="grid lg:flex gap-10">
-    {socialMedia.map((social, index) => (
-      <a key={index} target="_blank" href={social.href} className="">
-        <social.icon
-          className="fill-slate-700 cursor-pointer hover:fill-white ease-in duration-100"
-          size={social.size}
-        />
-      </a>
-    ))}
-  </div>
-);
+// export const SocialMediaLinks: React.FC = () => (
+//   <div className="grid lg:flex gap-10">
+//     {socialMedia.map((social, index) => (
+//       <a key={index} target="_blank" href={social.href} className="">
+//         <social.icon
+//           className="fill-slate-700 cursor-pointer hover:fill-white ease-in duration-100"
+//           size={social.size}
+//         />
+//       </a>
+//     ))}
+//   </div>
+// );
